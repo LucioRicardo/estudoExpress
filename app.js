ca8a420fd16c2dde08f0e09bdb0b170284f06cc3
LucioRicardo <lucio.ribeiroaraujo@gmail.com>
2021-03-31 10:16:29 -0300

    Servidor rodando e nodemon

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
 const app = express();
 
-console.log(app);
+// console.log(app);
+
+app.get('/', (req, res) => {
+    res.send('Olá, Pessoal!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!')
+});
