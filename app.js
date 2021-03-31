8127ecdf88b20ac1f1c03c5279b0f82c46ee157e
LucioRicardo <lucio.ribeiroaraujo@gmail.com>
2021-03-31 14:45:53 -0300

    Testes com o CRUD e uuid

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const { v4:uuidv4 } = require('uuid');
+
 const app = express();
 
-// console.log(app);
+app.use(express.json());
+
+const projetos = [];
 
 app.get('/projetos', (req, res) => {
-    return res.json([
-        'Projeto 1',
-        'Projeto 2'
-    ]);
+    //const { title, owner } = req.query;
+    
+    return res.json(projetos);
 });
 
 app.post('/projetos', (req, res) => {
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const { title, owner } = req.body;
+
+    const projeto = { id: uuidv4(), title, owner };
+
+    projetos.push(projeto);
+
+    return res.json(projeto);
 });
 
 app.put('/projetos/:id', (req, res) => {
+    const params = req.params;
+
+    console.log(params);
+    
     return res.json([
         'Projeto 51',
         'Projeto 2',
