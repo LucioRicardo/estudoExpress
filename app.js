0b26644f6cb596c4107635388a2be3d6145b7a9e
LucioRicardo <lucio.ribeiroaraujo@gmail.com>
2021-03-31 11:32:55 -0300

    CRUD criado

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ const app = express();
 
 // console.log(app);
 
-app.get('/', (req, res) => {
-    res.send('Olá, Pessoal!');
+app.get('/projetos', (req, res) => {
+    return res.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
+});
+
+app.post('/projetos', (req, res) => {
+    return res.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.put('/projetos/:id', (req, res) => {
+    return res.json([
+        'Projeto 51',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.delete('/projetos/:id', (req, res) => {
+    return res.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
 });
 
 app.listen(3000, () => {
